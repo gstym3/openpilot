805eb6bc7db31e56ba94df444fb40d6e80e60ed8
gstym3 <132193294+gstym3@users.noreply.github.com>
2024-04-03 07:42:49 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -84,13 +84,13 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
   # The Dynamic follow function is adjusted by Marc(cgw1968-5779)
   if personality==log.LongitudinalPersonality.relaxed:
     x_vel =  [0,   19.45,   25,    31.95,   41.67]
-    y_dist = [1.0, 1.0,   1.2,   1.4,   1.45]
+    y_dist = [0.97, 0.97,   1.2,   1.42,   1.45]
   elif personality==log.LongitudinalPersonality.standard:
     x_vel =  [0,   19.45,   25,    31.95,   41.67]
-    y_dist = [1.0, 1.0,   1.2,   1.4,   1.45]
+    y_dist = [0.97, 0.97,   1.2,   1.42,   1.45]
   elif personality==log.LongitudinalPersonality.aggressive:
     x_vel =  [0,   19.45,   25,    31.95,   41.67]
-    y_dist = [1.0, 1.0,   1.2,   1.4,   1.45]
+    y_dist = [0.97, 0.97,   1.2,   1.42,   1.45]
   else:
     raise NotImplementedError("Dynamic Follow personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
@@ -98,11 +98,11 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
 
 def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 4.5
+    return 4.3
   elif personality==log.LongitudinalPersonality.standard:
-    return 4.5
+    return 4.3
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 4.0
+    return 4.3
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
